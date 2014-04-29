f36a3641da655bd3ef59f7324e586b6edc3bc3c8
Calen Pennington <cale@edx.org>
2014-04-29 16:30:48 -0400

    Send and receive using the Event type

--- src/Network/ZeroRPC/ZeroRPC.hs
+++ src/Network/ZeroRPC/ZeroRPC.hs
@@ -2,16 +2,33 @@
 
 module Network.ZeroRPC where
 
+import Control.Applicative ((<$>), (<*>))
 import System.ZMQ4.Monadic (runZMQ, socket, connect, send, receive, Req(..))
-import Data.MessagePack (Object, pack, unpack, Assoc(..))
+import Data.MessagePack (pack, unpack, toObject, Object(..), OBJECT, Packable, from, Unpackable(..))
 import Data.Text (Text)
 import Data.ByteString.Lazy (toStrict)
 import Text.Show.Pretty (ppShow)
 
-type Header = [(Object, Object)]
+type Header = (Object, Object)
 type Name = Text
 
-data Event a = Event Header Name a
+data Event a = Event [Header] Name a
+    deriving Show
+
+instance (Packable a) => Packable (Event a) where
+    from (Event hs n v) = from (ObjectMap hs, n, v)
+
+instance (Unpackable a) => Unpackable (Event a) where
+    get = do
+        (ObjectMap headers, name, value) <- get
+        return $ Event headers name value
+
+event :: Name -> a -> Event a
+event = Event []
+
+(.=) :: (OBJECT k, OBJECT v) => k -> v -> Header
+key .= value = (toObject key, toObject value)
+{-# INLINE (.=) #-}
 
 heartbeat = Event [] "_zpc_hb" ()
 more = Event [] "_zpc_more"
@@ -19,10 +36,16 @@ more = Event [] "_zpc_more"
 stream = Event [] "STREAM"
 streamDone = Event [] "STREAM_DONE" ()
 
+msgId :: Text -> Header
+msgId i = ("message_id" :: Text) .= i
+
+version3 :: Header
+version3 = ("v" :: Text) .= (3 :: Int)
+
 ping serverName = return ("pong", serverName)
 
-inspect :: (Assoc [(String, String)], String, [String])
-inspect = (Assoc [("message_id", "foo"), ("v", "3")], "_zerorpc_inspect", [])
+inspect :: Event [String]
+inspect = Event [msgId "foo", version3] "_zerorpc_inspect" []
 
 main = do
     value <- runZMQ $ do
@@ -31,4 +54,4 @@ main = do
         send req [] (toStrict $ pack inspect)
         result <- receive req
         return $ unpack result
-    putStr $ ppShow (value :: Object)
+    putStr $ ppShow (value :: Event Object)
